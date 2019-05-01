e05fd6e842ae6856a0797e3949e82b42e8196d8e
park seung kyun <psg9710@naver.com>
2019-05-01 14:25:31 +0900

    10220 self representing Seq
    Signed-off-by: park seung kyun <psg9710@naver.com>

---
+++ BOJ/21. 수학/10220 self representing Seq.cpp
@@ -0,0 +1,17 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int T,N;
+//	cin >> T;
+//	while (T--) {
+//		cin >> N;
+//		if (6 % N == 0)
+//			printf("0\n");
+//		else if (N == 4)
+//			printf("2\n");
+//		else
+//			printf("1\n");
+//	}
+//}
