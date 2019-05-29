9ed7d0be28c76c5fdc4bc37cc1c7cac73123a56b
kyun1016 <psg9710@naver.com>
2019-05-29 16:07:35 +0900

    1786 찾기
    Signed-off-by: kyun1016 <psg9710@naver.com>

--- BOJ/27. 문자열 사용하기 (중간)/1786 찾기.cpp
+++ BOJ/27. 문자열 사용하기 (중간)/1786 찾기.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+string T, P;
+
 vector<int> getPartialMatch(const string& N) {
 	int m = N.size();
 	vector<int> pi(m, 0);
@@ -28,54 +30,48 @@ vector<int> getPartialMatch(const string& N) {
 	return pi;
 }
 
-
-////코드 20.2 커누스-모리스-프렛(Knuth-Morris-Pratt) 문자열 검색 알고리즘의 구현
-
-//'짚더미'H의 부분 문자열로 '바늘' N이 출현하는 시작 위치들을 모두반환한다.
-vector<int> kmpSearch(const string& H, const string& N) {
+vector<int> kmpSearch2(const string& H, const string& N) {
 	int n = H.size(), m = N.size();
 	vector<int> ret;
-	//pi[i]=N[..i]의 접미사도 되고 접두사도 되는 문자열의 최대 길이
 	vector<int> pi = getPartialMatch(N);
-
-	//begin=matched=0에서부터 시작하자.
-	int begin = 0, matched = 0;
-	while (begin <= n - m) {
-		//만약 짚더미의 해당 글자가 바늘의 해당 글자와 같다면
-		if (matched < m && H[begin + matched] == N[matched]) {
-			++matched;
-			//결과적으로 m글자가 모두 일치했다면 답에 추가한다.
-			if (matched == m) ret.push_back(begin);
-		}
-		else {
-			//예외: matched가 0인 경우에는 다음 칸에서부터 계속
-			if (matched == 0)
-				begin++;
-			else {
-				begin += matched - pi[matched - 1];
-				//begin을 옮겼다고 처음부터 다시 비교할 필요가 없다.
-				//옮긴 ㅎ에도 pi[matched-1]만큼은 항상 일치하기 때문이다.
+	//현재 대응된 글자의 수
+	int matched = 0;
+	//짚더미의 각 글자를 순회한다.
+	for (int i = 0; i < n; i++) {
+		//matched번 글자와 짚더미의 해당 글자가 불일치할 경우,
+		//현재 대응된 글자의 수를 pi[matched-1]로 줄인다.
+		while (matched > 0 && H[i] != N[matched])
+			matched = pi[matched - 1];
+		//글자가 대응될 경우
+		if (H[i] == N[matched]) {
+			matched++;
+			if (matched == m) {
+				ret.push_back(i - m + 1);
 				matched = pi[matched - 1];
 			}
 		}
 	}
-
 	return ret;
-
 }
 
 
 
 int main() {
-	string T, P;
+	/*ios_base::sync_with_stdio(0);
+	cin.tie(0);*/
+
 	getline(cin, T);
 	getline(cin, P);
 
-	vector<int> ret = kmpSearch(T, P);
+	/*cout << T << endl;
+	cout << P << endl;*/
+	
+	vector<int> ret;
+	ret = kmpSearch2(T, P);
 
-	cout << ret.size() << endl;
+	cout << ret.size() << "\n";
 	for (int i = 0; i < ret.size(); i++) {
-		cout << ret[i] + 1 << endl;
+		cout << ret[i] + 1 << "\n";
 	}
 
 	return 0;
