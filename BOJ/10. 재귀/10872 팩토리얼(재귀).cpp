4a8513d5a3a51f4108b5ea688180286db3c16b5a
psg9710@office.uos.ac.kr <psg9710@office.uos.ac.kr>
2021-04-28 18:09:16 +0900

    10872 팩토리얼(재귀 활용 풀이)

---
+++ BOJ/10. 재귀/10872 팩토리얼(재귀).cpp
@@ -0,0 +1,16 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int build_factorial(int a) {
+//	if (a < 1)
+//		return 1;
+//	return build_factorial(a - 1)*a;
+//}
+//
+//int main() {
+//	int N;
+//	cin >> N;
+//
+//	cout << build_factorial(N) << endl;
+//}
