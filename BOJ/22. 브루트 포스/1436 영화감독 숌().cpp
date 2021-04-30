78fe3d3c68a3cc143390eaa6a2e9c13fe45506ed
psg9710@office.uos.ac.kr <psg9710@office.uos.ac.kr>
2021-04-30 19:32:30 +0900

    오래간만에 동적계획

---
+++ BOJ/22. 브루트 포스/1436 영화감독 숌().cpp
@@ -0,0 +1,31 @@
+//#include<iostream>
+//#include<algorithm>
+//#include<vector>
+//
+//using namespace std;
+//
+//int N;
+//vector<int> arr;
+//
+//int main() {
+//	//input
+//	cin >> N;
+//
+//	//calc
+//	for (int i = 0; i < 10000000; i++) {
+//		int temp = i;
+//		int count = 0;
+//		while (temp) {
+//			if (temp % 1000 == 666) {
+//				arr.push_back(i);
+//				break;
+//			}
+//			temp /= 10;
+//		}
+//	}
+//
+//	sort(arr.begin(), arr.end());
+//
+//	//output
+//	cout << arr[N-1] << endl;
+//}
